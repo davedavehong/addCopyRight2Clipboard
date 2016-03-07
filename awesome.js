7c28ace877b96b371c0053a8d89b865ac05961b3
davedavehong <davedavehong@qq.com>
2016-03-07 21:38:52 +0800

    改善pageA

--- awesome.js
+++ awesome.js
@@ -1,18 +1,16 @@
 /**
  * Created by dave on 3月4 004.
  */
-var localUrl = window.location.host;
-function setClipboardText(event) {
+var localUrl = window.location.href;
+function setClipboardText(event,nodeClip) {
     event.preventDefault();
-    var node = document.createElement('div');
-    node.appendChild(window.getSelection().getRangeAt(0).cloneContents());
     var htmlData = '商业转载请联系作者获得授权，非商业转载请注明出处。<br />'
-        + '作者：dave<br />链接：<a href="http://'+localUrl+'">'+localUrl+'</a><br />'
+        + '作者：dave<br />链接：<a href="http://' + localUrl + '">' + localUrl + '</a><br />'
         + '来源：<a href="http://baidu.com">elephstor.com</a><br /><br />'
-        + node.innerHTML
+        + nodeClip.innerHTML
         + '</div>';
     var textData = '商业转载请联系作者获得授权，非商业转载请注明出处。\n'
-        + '作者：dave\n链接：'+localUrl+'\n'
+        + '作者：dave\n链接：' + localUrl + '\n'
         + '来源：elephstor\n'
         + window.getSelection().getRangeAt(0);
     if (event.clipboardData) {
@@ -42,7 +40,11 @@ function showCopyRightMsg(event) {
 }
 var pageA = document.getElementById("pageA");
 pageA.addEventListener('copy', function (e) {
-    setClipboardText(e);
+    var nodeClip = document.createElement('div');
+    nodeClip.appendChild(window.getSelection().getRangeAt(0).cloneContents());
+    if (nodeClip.innerText.length > 24) {
+        setClipboardText(e,nodeClip);
+    }
 });
 
 var pageB = document.getElementById('pageB');
